715d0da250bc38497a39235b5ff1042322b89dd7
Ahmet Kaan Toprakçıoğlu <56438853+ahmettoprakcioglu@users.noreply.github.com>
2025-01-19 12:56:14 +0300

    feat: enhance step management in CreateLogo component; add activeStep state and useEffect for dynamic step tracking

--- app/create/page.jsx
+++ app/create/page.jsx
@@ -1,6 +1,6 @@
   'use client'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import LogoTitle from './_components/LogoTitle';
 import { ArrowLeft, ArrowRight } from 'lucide-react';
 import { Button } from '@/components/ui/button';
@@ -13,14 +13,32 @@ import PricingModel from './_components/PricingModel';
 const CreateLogo = () => {
   const [step, setStep] = useState(1);
   const [formData, setFormData] = useState({});
+  const [activeStep, setActiveStep] = useState('title');
   const onHandleInputChange = (field, value = '') => {
     setFormData(prev => ({
       ...prev,
       [field]: value
     }));
   }
-
-  console.log('formData: ', formData);
+  console.log("formData: ", formData);
+  
+  useEffect(() => {
+    if (step === 1) {
+      setActiveStep('title');
+    } else if (step === 2) {
+      setActiveStep('desc')
+    } else if (step === 3) {
+      setActiveStep('palette')
+    } else if (step === 4) {
+      setActiveStep('design')
+    } else if (step === 5) {
+      setActiveStep('idea')
+    } else if (step === 6) {
+      setActiveStep('pricing')
+    } else {
+      setActiveStep('')
+    }
+  }, [step]);
 
   return (
     <div className='mt-28 p-10 border rounded-xl 2xl:mx-72'>
@@ -44,7 +62,10 @@ const CreateLogo = () => {
             Previous
           </Button>
         )}
-        <Button onClick={() => setStep(curr => curr + 1)}>
+        <Button
+          onClick={() => setStep(curr => curr + 1)}
+          disabled={!formData[activeStep]}
+        >
           <ArrowRight />
           Continue
         </Button>
